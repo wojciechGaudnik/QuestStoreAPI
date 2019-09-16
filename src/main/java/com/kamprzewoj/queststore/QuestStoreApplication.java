7972e5716dbd6f943ba6be8c175450f8aecbbfe2
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-09-16 16:41:45 +0200

    Ceaning POM, JSON default

--- src/main/java/com/kamprzewoj/queststore/QuestStoreApplication.java
+++ src/main/java/com/kamprzewoj/queststore/QuestStoreApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
-@SpringBootApplication
+@SpringBootApplication()
 public class QuestStoreApplication {
 
 	public static void main(String[] args) {
