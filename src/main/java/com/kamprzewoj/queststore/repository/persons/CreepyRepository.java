fcee55e55ac964a69a9252676672459e853db011
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-09-26 22:45:50 +0200

    Test

--- src/main/java/com/kamprzewoj/queststore/repository/persons/CreepyRepository.java
+++ src/main/java/com/kamprzewoj/queststore/repository/persons/CreepyRepository.java
@@ -4,7 +4,7 @@ import com.kamprzewoj.queststore.model.persons.Creepy;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//test
+//test test 2
 @RepositoryRestResource
 public interface CreepyRepository extends CrudRepository<Creepy, Long> {
 }
