02148800f5555ca812e6308368cb0a4cfec57a1e
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-10-03 18:25:50 +0200

    BUG empty embedded redy for test

--- src/main/java/com/kamprzewoj/queststore/controllers/CreepyController.java
+++ src/main/java/com/kamprzewoj/queststore/controllers/CreepyController.java
@@ -6,12 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.core.EmbeddedWrapper;
+import org.springframework.hateoas.core.EmbeddedWrappers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -25,13 +28,20 @@ public class CreepyController {
 		this.creepyService = creepyService;
 	}
 
-	@Procedure("application/hal+json")
+	@Procedure("application/json+hal")
 	@GetMapping(value = "/getAllMentors/")
-	public ResponseEntity<Resources<User>> getAllUsers() {
+	public ResponseEntity getAllUsers() {
 		final List<User> userList = creepyService.getAllMentors();
-		final Resources<User> resources = new Resources<>(userList);
-		String uriString = ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString();
-		resources.add(new Link(uriString, "self"));
-		return ResponseEntity.ok(resources);
+		if (userList.isEmpty()) {
+			EmbeddedWrappers wrappers = new EmbeddedWrappers(false);
+			EmbeddedWrapper wrapper = wrappers.emptyCollectionOf(User.class);
+			Resources<Object> resources = new Resources<>(Arrays.asList(wrapper));
+			return ResponseEntity.ok(resources);
+		} else {
+			final Resources<User> resources = new Resources<>(userList);
+			String uriString = ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString();
+			resources.add(new Link(uriString, "self"));
+			return ResponseEntity.ok(resources);
+		}
 	}
 }
